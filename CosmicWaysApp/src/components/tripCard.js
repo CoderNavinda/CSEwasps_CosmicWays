1050648989c77f2ce77fdb33410c37bad1842c06
Kavindu Nilshan <kavindunilshanliyanage@gmail.com>
2023-08-20 16:15:25 +0530

    changed

--- CosmicWaysApp/src/components/tripCard.js
+++ CosmicWaysApp/src/components/tripCard.js
@@ -1,76 +1,80 @@
-import { Dimensions, Image, StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from 'react-native';
+import { Dimensions, Image, StyleSheet, Text, View } from 'react-native';
 
-const { width, height } = Dimensions.get("window");
 
-export default function TripCard() {
+const { width, height } = Dimensions.get('window');
+
+export default function TripCard({start, dest, startCity, destCity, date, time, flight, durationDays, durationHours}) {
+
+    const handleClick = () => {
+      console.log("ok with it");
+    };
+
   return (
-    <View>
-      {/* Flight tip backgroud */}
-      <View style={styles.blueBox} />
+         <TouchableOpacity onPress={handleClick}>
+              {/* Flight tip backgroud */}
+              <View style={styles.blueBox} />
 
-      <View style={{ flexDirection: "row" }}>
-        <View style={styles.leftElipse} />
-        <View style={styles.rightElipse} />
-      </View>
+              <View style={{ flexDirection: 'row' }}>
+                <View style={styles.leftElipse} />
+                <View style={styles.rightElipse} />
+              </View>
 
-      <View style={styles.cardBottomText}>
-        <Text style={styles.cdLabel}>Date &amp; time</Text>
-        <Text style={styles.cdDateTime}>Aug 20, 0430h</Text>
-        <Text style={styles.cdFlightNumberLabel}>Flight Number</Text>
-        <Text style={styles.cdFlightNumber}>AB889</Text>
-      </View>
+              <View style={styles.cardBottomText}>
+                <Text style={styles.cdLabel}>Date &amp; time</Text>
+                <Text style={styles.cdDateTime}>{date}, {time}</Text>
+                <Text style={styles.cdFlightNumberLabel}>Flight Number</Text>
+                <Text style={styles.cdFlightNumber}>{flight}</Text>
+              </View>
 
-      <View style={styles.cardTopText}>
-        <Text style={styles.cdTitleEarth}>Earth</Text>
-        <View style={styles.cdTitle}>
-          <Text style={styles.cdSubTitleLeft}>Colombo</Text>
-          <Text style={styles.cdSubTitleRight}>Olympus Mons</Text>
-        </View>
-        <Text style={styles.cdTitleMars}>Mars</Text>
-        <Text style={styles.cdTime}>1d/{"\n"}23h</Text>
-      </View>
+              <View style={styles.cardTopText}>
+                <Text style={styles.cdTitleEarth}>{start}</Text>
+                <View style={styles.cdTitle}>
+                  <Text style={styles.cdSubTitleLeft}>{startCity}</Text>
+                  <Text style={styles.cdSubTitleRight}>{destCity}</Text>
+                </View>
+                <Text style={styles.cdTitleMars}>{dest}</Text>
+                <Text style={styles.cdTime}>{durationDays}d/{"\n"}{durationHours}h</Text>
+              </View>
 
-      <View style={styles.cdLine} />
+              <View style={styles.cdLine} />
 
-      <View style={styles.RocketImage}>
-        <Image
-          source={require("../../assets/Route.png")}
-          style={styles.image}
-        />
-      </View>
-    </View>
+            <View style={styles.RocketImage}>
+              <Image source={require('../assets/Route.png')} style={styles.image} />
+            </View>
+        </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
-  blueBox: {
-    flex: 1,
-    width: width * 0.9,
+    blueBox: {
+    flex:1,
+    width: width*0.9,
     height: height * 0.244,
     left: width * 0.05,
     borderRadius: 15,
     top: height * 0.2,
-    backgroundColor: "#12092F",
-    position: "absolute",
+    backgroundColor: '#12092F',
+    position: 'absolute',
   },
 
   leftElipse: {
-    position: "absolute",
+    position:'absolute',
     width: width * 0.05,
     height: height * 0.02,
     left: width * 0.026,
-    top: height * 0.362,
+    top: height*0.362,
     borderRadius: 25,
-    backgroundColor: "#1C0E57",
+    backgroundColor: '#1C0E57', 
   },
   rightElipse: {
-    position: "absolute",
+    position:'absolute',
     width: width * 0.05,
     height: height * 0.02,
     left: width * 0.92,
-    top: height * 0.362,
+    top: height*0.362,
     borderRadius: 25,
-    backgroundColor: "#1C0E57",
+    backgroundColor: '#1C0E57',
   },
 
   cardBottomText: {
@@ -78,53 +82,54 @@ const styles = StyleSheet.create({
     height: height * 0.1,
     left: width * 0.114,
     top: height * 0.376,
-    position: "absolute",
+    position: 'absolute',
   },
   cdLabel: {
-    width: width * 0.23,
+    width: width * 0.3,
     left: 0,
     top: 0,
-    position: "absolute",
-    color: "#8C8A93",
+    position: 'absolute',
+    color: '#8C8A93',
     fontSize: 14,
-    fontWeight: "400",
-    textTransform: "uppercase",
+    fontFamily: 'Roboto',
+    fontWeight: '400',
+    textTransform: 'uppercase',
     lineHeight: height * 0.022,
   },
   cdDateTime: {
-    width: width * 0.3,
+    width: width * 0.30,
     left: 0,
     top: height * 0.028,
-    position: "absolute",
-    color: "#EAEDFA",
+    position: 'absolute',
+    color: '#EAEDFA',
     fontSize: 14,
-    fontFamily: "Roboto",
-    fontWeight: "400",
+    fontFamily: 'Roboto',
+    fontWeight: '400',
     lineHeight: height * 0.022,
   },
   cdFlightNumberLabel: {
     width: width * 0.31,
     left: width * 0.47,
     top: 0,
-    position: "absolute",
-    textAlign: "right",
-    color: "#8C8A93",
+    position: 'absolute',
+    textAlign: 'right',
+    color: '#8C8A93',
     fontSize: 14,
-    fontFamily: "Roboto",
-    fontWeight: "400",
-    textTransform: "uppercase",
+    fontFamily: 'Roboto',
+    fontWeight: '400',
+    textTransform: 'uppercase',
     lineHeight: height * 0.022,
   },
   cdFlightNumber: {
     width: width * 0.31,
     left: width * 0.47,
     top: height * 0.028,
-    position: "absolute",
-    textAlign: "right",
-    color: "#EAEDFA",
+    position: 'absolute',
+    textAlign: 'right',
+    color: '#EAEDFA',
     fontSize: 14,
-    fontFamily: "Roboto",
-    fontWeight: "400",
+    fontFamily: 'Roboto',
+    fontWeight: '400',
     lineHeight: height * 0.022,
   },
   cardTopText: {
@@ -132,65 +137,65 @@ const styles = StyleSheet.create({
     height: height * 0.1,
     left: width * 0.114,
     top: height * 0.256,
-    position: "absolute",
+    position: 'absolute',
   },
   cdSubTitleLeft: {
     width: width * 0.26,
     left: 0,
     top: height * 0.046,
-    position: "absolute",
-    color: "#EAEDFA",
+    position: 'absolute',
+    color: '#EAEDFA',
     fontSize: 14,
-    fontFamily: "Roboto",
-    fontWeight: "400",
+    fontFamily: 'Roboto',
+    fontWeight: '400',
     lineHeight: 17.36,
   },
   cdSubTitleRight: {
     width: width * 0.28,
     left: width * 0.5,
     top: height * 0.046,
-    position: "absolute",
-    textAlign: "right",
-    color: "#EAEDFA",
+    position: 'absolute',
+    textAlign: 'right',
+    color: '#EAEDFA',
     fontSize: 14,
-    fontFamily: "Roboto",
-    fontWeight: "400",
+    fontFamily: 'Roboto',
+    fontWeight: '400',
     lineHeight: 17.36,
   },
   cdTitle: {
-    flexDirection: "row",
+    flexDirection: 'row',
   },
 
   cdTitleEarth: {
     width: width * 0.17,
-    position: "absolute",
-    color: "white",
+    position: 'absolute',
+    color: 'white',
     fontSize: 22,
-    fontFamily: "Roboto",
-    fontWeight: "500",
+    fontFamily: 'Roboto',
+    fontWeight: '500',
     lineHeight: 27.28,
   },
   cdTitleMars: {
     width: width * 0.17,
     left: width * 0.62,
     top: 0,
-    position: "absolute",
-    color: "white",
+    position: 'absolute',
+    color: 'white',
     fontSize: 22,
-    fontFamily: "Roboto",
-    fontWeight: "500",
+    fontFamily: 'Roboto',
+    fontWeight: '500',
     lineHeight: 27.28,
   },
   cdTime: {
     width: width * 0.11,
     left: width * 0.33,
     top: height * 0.01,
-    position: "absolute",
-    textAlign: "center",
-    color: "#DCDEE3",
+    position: 'absolute',
+    textAlign: 'center',
+    color: '#DCDEE3',
     fontSize: 12,
-    fontFamily: "Roboto",
-    fontWeight: "400",
+    fontFamily: 'Roboto',
+    fontWeight: '400',
   },
 
   cdLine: {
@@ -198,10 +203,10 @@ const styles = StyleSheet.create({
     height: 1,
     left: width * 0.114,
     top: height * 0.356,
-    position: "absolute",
+    position: 'absolute',
     borderWidth: 1,
-    borderColor: "white",
-    borderStyle: "dotted",
+    borderColor: 'white',
+    borderStyle: 'dotted',
   },
 
   RocketImage: {
