a5c99cbf92f996c4b53ab289d5aaf801ab3922ea
CoderNavinda <72337893+CoderNavinda@users.noreply.github.com>
2023-08-20 11:43:22 +0530

    register screen done. need some fine tuning

--- CosmicWaysApp/App.js
+++ CosmicWaysApp/App.js
@@ -1,44 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet,
-        Dimensions,
-        Platform, 
-        Text,
-        TouchableWithoutFeedback ,
-        TouchableOpacity,
-        View,
-        Image,
-        SafeAreaView,
-        Button,
-        Alert
-        } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Dimensions,
+  Platform,
+  Text,
+  TouchableWithoutFeedback,
+  TouchableOpacity,
+  View,
+  Image,
+  SafeAreaView,
+  Button,
+  Alert,
+} from "react-native";
 
-
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './app/screens/LoginScreen';
-        
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./app/screens/LoginScreen";
+import RegisterScreen from "./app/screens/RegisterScreen";
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-
-<NavigationContainer>
-<Stack.Navigator screenOptions={{headerShown: false}}>
-  <Stack.Screen name="LoginScreen" component={LoginScreen} />
-</Stack.Navigator>
-</NavigationContainer>
-
-
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="RegisterScreen" component={RegisterScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
-
-
-
-
-
   // container: {
   //   flex: 1,
   //   backgroundColor: '#250E67',
@@ -47,15 +39,9 @@ const styles = StyleSheet.create({
   //   paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   // },
   // buttonstyle : {
-  //   alignItems: 'center', // Center the image horizontally 
+  //   alignItems: 'center', // Center the image horizontally
   //   backgroundColor: '#FFFFF',
   // }
 });
 
-
-
 // In App.js in a new project
-
-
-
-
