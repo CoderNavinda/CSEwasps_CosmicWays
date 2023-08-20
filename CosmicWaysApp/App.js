55a3f4cf5f349e7d95371af386e9f53c2a035ecf
Somesh Chandimal <someshchandih@gmail.com>
2023-08-20 09:36:59 +0530

    config firebase

--- CosmicWaysApp/App.js
+++ CosmicWaysApp/App.js
@@ -1,10 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect } from "react";
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { db, collection, getDocs } from "./firebase.config.js";
 
 export default function App() {
+  async function testFirebase() {
+    const postsCol = collection(db, "destinations");
+    const postSnapshot = await getDocs(postsCol);
+    const postList = postSnapshot.docs.map((doc) => doc.data());
+    console.log(postList);
+  }
+  useEffect(() => {
+    testFirebase();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>shit!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +24,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
